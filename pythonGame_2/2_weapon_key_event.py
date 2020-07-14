2d1ef8d196cc006fac6c5f017be4d813a83f0611
ssorry123 <syd03160@gmail.com>
2020-07-15 00:36:23 +0900

    클래스화 작업중

--- pythonGame_2/2_weapon_key_event.py
+++ pythonGame_2/2_weapon_key_event.py
@@ -4,50 +4,53 @@ import random
 import os
 
 ''' 게임 필수 세팅 '''
-# 반드시 해야함
-pygame.init()
-# 화면 크기 설정
-screen_width = 640 
-screen_height = 480
-screen = pygame.display.set_mode(
-        (screen_width, screen_height)
-    )
+class NadoGame:
+	def __init__(self):
+		# 초기화
+		pygame.init()
+		# 화면 설정
+		self.screen_width = 640
+		self.screen_height = 480
+		self.screen = pygame.display.set_mode(
+			(self.screen_width, self.screen_height)
+		)
+		pygame.display.set_caption("Nado Pang")
+		self.background = pygame.image.load("./image/background_1.png")
+		self.stage = pygame.image.load("./image/stage.png")
+		self.stage_size = self.stage.get_rect().size
+		self.stage_height = self.stage_size[1]
+		
+		self.total_time = 150
 
+class Unit:
+	def __init__(self, image_path):
+		self.image = pygame.image.load(image_path)	# 이미지 경로
+		self.size = self.image.get_rect().size		# 이미지 크기
+		self.width = self.size[0]						# 이미지 너비
+		self.height = self.size[1]					# 이미지 높이
 
-''' 게임 기본 세팅 '''
-# 화면 타이틀 설정
-pygame.display.set_caption("Nado Pang")
-# 배경 이미지 불러오기
-background = pygame.image.load("./image/background.png")
-# 스테이지 이미지 불러오기
-stage = pygame.image.load("./image/stage.png")
-stage_size = stage.get_rect().size
-stage_height = stage_size[1]    # stage 높이 계산
+class Character(Unit):
+	def __init__(self, image_path):
+		# 이미지 설정
+		Unit.__init__(self, image_path)
+		# 시작위치 설정
+		self.x_pos = myGame.screen_width/2 - self.width/2
+		self.y_pos = myGame.screen_height - self.height - myGame.stage_height
+		# 움직임 설정
+		self.to_x = 0
+		self.speed = 5
+		
+class Weapon(Unit):
+	weapons = list()
+	def __init__(self, image_path):
+		Unit.__init__(self,image_path)
+		self.speed = 13
+		
 
-# 게임 시간 설정
-total_time = 150
+myGame = NadoGame()
+character = Character("./image/character.png")
+weapon = Weapon("./image/weapon.png")
 
-
-''' 주인공 캐릭터 설정 '''
-# 캐릭터(스프라이터) 불러오기
-character = pygame.image.load("./image/character.png")
-character_size = character.get_rect().size # 이미지의 크기 구하기
-character_width = character_size[0]
-character_height = character_size[1]
-# 시작 위치
-character_x_pos = (screen_width / 2) - (character_width / 2) 
-character_y_pos = screen_height - character_height  - stage_height
-# 이동 조작
-character_to_x = 0      # 좌우로만 이동
-character_speed = 5     # 캐릭터 속도
-
-''' 무기 설정 '''
-weapon = pygame.image.load("./image/weapon.png")
-weapon_size = weapon.get_rect().size
-weapon_width = weapon_size[0]
-weapon_speed = 10
-
-weapons = list()
 
 
 ''' 게임 시작 전 세팅 '''
@@ -62,7 +65,7 @@ kdown_left = False
 while running :
     # 남은 시간 계산
     elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000   # ms / 1000
-    eta_time = (int)(total_time - elapsed_time)
+    eta_time = (int)(myGame.total_time - elapsed_time)
     if eta_time < 0 :
         running = False
         continue
@@ -71,14 +74,14 @@ while running :
 
     ''' drawing '''
     # 배경 그리기
-    screen.blit(background, (0,0)) #screen.fill((122,10,122))
+    myGame.screen.blit(myGame.background, (0,0)) #screen.fill((122,10,122))
     # 무기 그리기
     for weapon_x_pos, weapon_y_pos in weapons:
-        screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+        myGame.screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
     # stage 그리기
-    screen.blit(stage, (0 , screen_height - stage_height))
+    myGame.screen.blit(myGame.stage, (0 , myGame.screen_height - myGame.stage_height))
     # 캐릭터 그리기
-    screen.blit(character, (character_x_pos, character_y_pos))
+    myGame.screen.blit(character.image, (character.x_pos, character.y_pos))
 
     # 게임 화면을 다시 그리기( 계속 해야함 )
     pygame.display.update()
@@ -97,14 +100,14 @@ while running :
             if event.key == pygame.K_LEFT:         # 좌이동
                 kdown_left = True
                 #kdown_right = False
-                character_to_x = -character_speed
+                character.to_x = -character.speed
             elif event.key == pygame.K_RIGHT:      # 우이동
                 kdown_right = True
                 #kdown_left = False
-                character_to_x = +character_speed
+                character.to_x = +character.speed
             elif event.key == pygame.K_SPACE:      # space
-                weapon_x_pos = character_x_pos + character_width/2 - weapon_width/2
-                weapon_y_pos = character_y_pos
+                weapon_x_pos = character.x_pos + character.width/2 - weapon_width/2
+                weapon_y_pos = character.y_pos
                 weapons.append([weapon_x_pos, weapon_y_pos])
         # key up, setting flag
         elif event.type == pygame.KEYUP:
@@ -112,28 +115,30 @@ while running :
                 kdown_left = False
             elif event.key == pygame.K_RIGHT:
                 kdown_right = False
-        # char_pos setting with flag
+        
+        # char_pos setting with flag, 기존 코드 개선 부분
         if (kdown_left == False) and (kdown_right == False):
-            character_to_x = 0
+            character.to_x = 0
         elif (kdown_left == False) and (kdown_right == True):
-            character_to_x = character_speed
+            character.to_x = character.speed
         elif (kdown_left == True) and (kdown_right == False):
-            character_to_x = -character_speed
+            character.to_x = -character.speed
         
     print((str)(kdown_left) + " " + (str)(kdown_right))
     # fps, fps별 속도 보정
     dt = clock.tick(60)
 
     ''' 캐릭터 위치 정보 업데이트 '''
-    character_x_pos += character_to_x
+    character.x_pos += character.to_x
 
-    if character_x_pos < 0:
-        character_x_pos = 0
-    elif character_x_pos > screen_width - character_width:
-        character_x_pos = screen_width - character_width
+    if character.x_pos < 0:
+        character.x_pos = 0
+    elif character.x_pos > myGame.screen_width - character.width:
+        character.x_pos = myGame.screen_width - character.width
 
     ''' 무기 정보 업데이트 '''
     # 무기가 위로 날라감
+    # w[1] --> weapon_y_pos
     weapons = [ [w[0], w[1] - weapon_speed] for w in weapons ]
     # 무기가 천장에 닿으면 사라짐
     # 조건에 맞는 것만 저장한다.
